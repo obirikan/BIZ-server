d45480d06a8adedf49cb568a4b948a5edd521b5f
obirikan <obirikan020@gmail.com>
2022-04-02 14:51:36 -0700

    make it better

--- routes/use.js
+++ routes/use.js
@@ -1,5 +1,5 @@
 const users=require('../models/users.js')
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const { exists } = require('../models/users.js')
 const router=require('express').Router()
